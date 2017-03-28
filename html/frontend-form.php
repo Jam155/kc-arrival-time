7dc540b24d603ec77c2c885f5f040c86510a52fa
James Lendrem <Jamlendrem@gmail.com>
2017-03-28 11:58:21 +0100

    Remove text about low seasons

--- html/frontend-form.php
+++ html/frontend-form.php
@@ -1,4 +1,4 @@
 <label for="arrival-time" class="top-label" style="margin-top: 10px;">Approximate arrival time:
 <input type="text" name="arrival-time" value="<?php echo $_POST['arrival-time'] ? $_POST['arrival-time'] : '' ; ?>" style="margin-top: 10px; margin-bottom: 10px;" />
-arrival from 4pm, or 3pm in Low Season (This is just a guide to help us when preparing the cottage for you)
+arrival from 4pm. (This is just a guide to help us when preparing the cottage for you.)
 </label>
